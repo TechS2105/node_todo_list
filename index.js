d2a748a36d073ad091e6ac486dfac3c85db53c80
TechS2105 <techsovan.github0521@gmail.com>
2025-02-24 19:03:22 +0530

    add files to stagging area

--- index.js
+++ index.js
@@ -1,69 +1,98 @@
-import express from "express";
-import bodyParser from "body-parser";
-import pg from "pg";
+import express from 'express';
+import bodyParser from 'body-parser';
+import pg from 'pg';
 
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
+
+  user: "postgres", 
+  host: "localhost", 
   database: "parmalist",
   password: "12345",
-  port: 5432,
+  port: 5432
+
 });
 
 db.connect();
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
-
-async function items() {
+async function getItems() {
+  
   const result = await db.query("SELECT * FROM items");
   let items = [];
   items = result.rows;
   return items;
+
 }
 
-app.get("/", async (req, res) => {
-  const userItems = await items();
-  res.render("index.ejs", { listTitle: "Today", listItems: userItems });
+app.get('/', async (req, res) => {
+  
+  const items = await getItems();
+  res.render('index.ejs', {listTitle: "Today", listItems: items});
+
 });
 
-app.post("/add", async (req, res) => {
-  const newItem = req.body.newItem;
+// add items 
+app.post('/add', async (req, res) => {
+  
+  const newItems = req.body.newItem;
+  
+  try { 
 
-  try {
-    await db.query("INSERT INTO items(title) VALUES($1)", [newItem]);
-    res.redirect("/");
+    await db.query("INSERT INTO items(title) VALUES($1)", [newItems]);
+    res.redirect('/');
+
   } catch (err) {
+    
     console.log(err);
+
   }
+
 });
 
-app.post("/edit", async (req, res) => {
+// update items
+app.post('/edit', async (req, res) => {
+  
   const id = req.body.updatedItemId;
-  const title = req.body.updatedItemTitle;
+  const item = req.body.updatedItemTitle;
 
   try {
-    await db.query("UPDATE items SET title = $1 WHERE id = $2", [title, id]);
-    res.redirect("/");
+    
+    await db.query("UPDATE items SET title = $1 WHERE id = $2", [item, id]);
+    res.redirect('/');
+
   } catch (err) {
+    
     console.log(err);
+
   }
+
 });
 
-app.post("/delete", async (req, res) => {
-  const deleteId = req.body.deleteItemId;
+// delete items 
+app.post('/delete', async (req, res) => {
+  
+  const id = req.body.deleteItemId;
 
-  try {
-    await db.query("DELETE FROM items WHERE id = $1", [deleteId]);
-    res.redirect("/");
+  try { 
+
+    await db.query("DELETE FROM items WHERE id = $1", [id]);
+    res.redirect('/');
+
   } catch (err) {
+    
     console.log(err);
+
   }
+
 });
 
 app.listen(port, () => {
+
   console.log(`Server is started on ${port} port`);
+
 });
